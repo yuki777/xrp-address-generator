6428c12eb5c75b0f9fef5708047a1c0fb56a84c0
Yuki Adachi <177159+yuki777@users.noreply.github.com>
2024-09-18 15:08:02 +0700

    Support only CLI

--- create.js
+++ create.js
@@ -1,9 +1,11 @@
 const keypairs = require('ripple-keypairs');
-const api      = require('ripple-address-codec');
-const secret   = keypairs.generateSeed();
-const keypair  = keypairs.deriveKeypair(secret);
-const address  = keypairs.deriveAddress(keypair.publicKey);
+const api = require('ripple-address-codec');
+
+const secret = keypairs.generateSeed();
+const keypair = keypairs.deriveKeypair(secret);
+const address = keypairs.deriveAddress(keypair.publicKey);
 const xaddress = api.classicAddressToXAddress(address, false)
+
 console.log(JSON.stringify({
   address: address,
   xaddress: xaddress,
